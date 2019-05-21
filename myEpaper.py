f17c8ccdf01d890660407f0eb68776dfcc09959b
samlewis02 <samlewis02@yahoo.co.uk>
2019-05-21 17:05:44 +0800

    New loop timings

--- myEpaper.py
+++ myEpaper.py
@@ -14,6 +14,20 @@ from bme280 import readBME280All
 myCalUrl = "https://script.google.com/macros/s/AKfycbwY2YIhEJeJc3GbmubJ4diF-R8mYYCfEiHH49LnxS70AvGRPskt/exec"
 myCurUrl = "http://data.fixer.io/api/latest?access_key=0a371f7901a6260a0ea11865f1ad98da&symbols=TWD,GBP&format=1"
 
+epd = epd4in2.EPD()
+epd.init()
+epd.Clear(0xff)
+
+# Drawing on the Vertical image
+Limage = Image.new('1', (epd4in2.EPD_HEIGHT, epd4in2.EPD_WIDTH), 0)  # 0: blacken the frame
+draw = ImageDraw.Draw(Limage)
+
+font48 = ImageFont.truetype('/usr/share/fonts/truetype/wqy/wqy-microhei.ttc', 48)
+font24 = ImageFont.truetype('/usr/share/fonts/truetype/wqy/wqy-microhei.ttc', 24)
+font16 = ImageFont.truetype('/usr/share/fonts/truetype/wqy/wqy-microhei.ttc', 16)
+
+
+
 def drawRectRnd(x0, y0, x1, y1, r, fill):
     draw.rectangle((x0+r,y0,x1-r,y1), fill=fill)
     draw.rectangle((x0,y0+r,x1,y1-r), fill=fill)
@@ -23,9 +37,15 @@ def drawRectRnd(x0, y0, x1, y1, r, fill):
     draw.ellipse((x1-2*r,y1-2*r,x1,y1), fill=fill)
 
 def initDisplay():
+ 
+    # draw background
     drawRectRnd(20, 30, 280, 100, 10, 255)
     drawRectRnd(20, 140, 280, 340, 10, 255)
-    epd.display(epd.getbuffer(Limage))
+    #epd.display(epd.getbuffer(Limage))
+    rn = datetime.datetime.now()
+    ltime = rn.strftime("%H:%M")
+    daydate = rn.strftime("%A, %d %B %Y")  
+    showTime(ltime, daydate)
     try:
         tdyStr, tomStr = getCal(myCalUrl)
         draw.text((30, 150), tdyStr + tomStr, font = font16, fill = 0)
@@ -44,46 +64,36 @@ def initDisplay():
         draw.text((30, 360), b'%.2f'%ltemp + b'C  ' + b'%.2f'%lhum + b'%RH  ' + b'%.1f'%lpres + b'hPa', font = font16, fill=255)
     except:
         draw.text((30, 360), "No T, RH, pressure data", font = font16, fill=255)
-         
+    # send buffer to display
+    epd.display(epd.getbuffer(Limage))    
     
 def showTime(ltime, daydate):
     drawRectRnd(20, 30, 280, 100, 10, 255) # blank area
-    draw.text((50, 40), ltime, font = font48, fill = 0)
+    draw.text((90, 40), ltime, font = font48, fill = 0)
     draw.rectangle((20,0,280,25), fill=0) # blank area
     draw.text((65, 5), daydate, font = font16, fill = 255)
-    epd.display(epd.getbuffer(Limage))
+    
     
 try:
-    epd = epd4in2.EPD()
-    epd.init()
-    epd.Clear(0xFF)
-    
-
-    # Drawing on the Vertical image
-    Limage = Image.new('1', (epd4in2.EPD_HEIGHT, epd4in2.EPD_WIDTH), 0)  # 255: clear the frame
-    draw = ImageDraw.Draw(Limage)
-    font48 = ImageFont.truetype('/usr/share/fonts/truetype/wqy/wqy-microhei.ttc', 48)
-    font24 = ImageFont.truetype('/usr/share/fonts/truetype/wqy/wqy-microhei.ttc', 24)
-    font16 = ImageFont.truetype('/usr/share/fonts/truetype/wqy/wqy-microhei.ttc', 16)
-
+    initDisplay()
+  
     t = time.time()
     time_now1 = t
     time_now2 = t
     time_now3 = t
-    initDisplay()
-    
+    this_min = time.localtime().tm_min
 
 ######################## START LOOP ########################
 
     while True:
-        if time.time() >= t+ 10: #every 10 seconds
+        
+        if time.localtime().tm_min != this_min: #every minute
+            #print("minute update")
             rn = datetime.datetime.now()
-            ltime = rn.strftime("%H:%M:%S")
+            ltime = rn.strftime("%H:%M")
+            #print(ltime)
             daydate = rn.strftime("%A, %d %B %Y")  
             showTime(ltime, daydate)
-            t = time.time()
-        # Crude scheduler to get T, RH, pressure every 10 secs
-        if time.time() >= time_now3 + 10:
             draw.rectangle((20,345,280,385), fill=0) # blank area
             try:
                 ltemp, lpres, lhum = readBME280All()
@@ -91,8 +101,11 @@ try:
 
             except:
                 draw.text((30, 360), "No T, R, P data", font = font16, fill = 0)
-            time_now3 = time.time()
+            #send buffer to display
+            epd.display(epd.getbuffer(Limage))
 
+            this_min = time.localtime().tm_min
+ 
         # Crude scheduler to get Google calendar every 30 mins
         if time.time() >= time_now1 + 1800:
             drawRectRnd(20, 140, 280, 340, 10, 255) # blank area
@@ -112,8 +125,9 @@ try:
             except:
                 draw.text((70, 110), "1GBP = "+ str(xrate) + "NT$", font = font16, fill = 255)
             time_now2 = time.time()
-   
+        time.sleep(5) # don't hog cpu
         
+                
 except:
     print('traceback.format_exc():\n%s',traceback.format_exc())
     exit()
